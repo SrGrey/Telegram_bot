8357218fb85c81fe6bdc8bffcbd8a4b545142178
SergioAvargin <sergey@ateneamobility.com>
2024-01-19 00:03:03 +0000

    Huge of refactoring (Logging, comments etc)

--- bot.py
+++ bot.py
@@ -1,12 +1,15 @@
 import io
 import os
+from typing import Union, List, Tuple
 
 import cv2
 import dlib
 from pydub import AudioSegment
+from pydub.exceptions import CouldntDecodeError
 
 import numpy as np
 import matplotlib.pyplot as plt
+
 from telethon import TelegramClient, events
 
 from dotenv import load_dotenv
@@ -14,18 +17,27 @@ from dotenv import load_dotenv
 load_dotenv(".env")
 
 import logging
-logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
-                    level=logging.WARNING)
+from colorama import Fore, Style
+
+
+# DEBUG_MODE using for show founded faces using matplotlib.pyplot and show more log info
+DEBUG_MODE = False
+
+logging.basicConfig(
+    format=f'{Fore.GREEN}[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s{Style.RESET_ALL}',
+    level=(logging.DEBUG if DEBUG_MODE else logging.INFO)
+)
+# Color settings for logging
+logging.addLevelName(logging.DEBUG, f'{Fore.CYAN}{logging.getLevelName(logging.DEBUG)}{Style.RESET_ALL}')
+logging.addLevelName(logging.INFO, f'{Fore.GREEN}{logging.getLevelName(logging.INFO)}{Style.RESET_ALL}')
+logging.addLevelName(logging.WARNING, f'{Fore.YELLOW}{logging.getLevelName(logging.WARNING)}{Style.RESET_ALL}')
+logging.addLevelName(logging.ERROR, f'{Fore.RED}{logging.getLevelName(logging.ERROR)}{Style.RESET_ALL}')
+
 
 api_id = int(os.getenv("API_ID"))
 api_hash = os.getenv("API_HASH")
 bot_token = os.getenv("BOT_TOKEN")
 
-bot_name = 'my_shiny_telegram_bot'
-bot_address = 't.me/my_shiny_telegram_bot'
-
-# DEBUG_MODE using for show founded faces using matplotlib.pyplot
-DEBUG_MODE = False
 
 client = TelegramClient('bot_session', api_id, api_hash).start(bot_token=bot_token)
 
@@ -39,15 +51,15 @@ async def handle_voice_message(event):
     try:
         if not os.path.exists(folder_path):
             os.makedirs(folder_path)
-    except Exception as e:
-        print(f"Error creating folder: {e}")
+    except OSError as e:
+        logging.error(f"Error creating folder: {e}")
     # Counting files for assign file number // change this method if files could be moved or replaced
     files = os.listdir(folder_path)
     output_path = f'{folder_path}/audio_message_{len(files)}.wav'
     try:
         await convert_to_wav(event, output_path)
-    except Exception as e:
-        print(f"Error handling voice message: {e}")
+    except CouldntDecodeError as e:
+        logging.error(f"Error handling voice message: {e}")
 
 
 async def convert_to_wav(event, output_path, sample_rate=16000):
@@ -61,8 +73,8 @@ async def convert_to_wav(event, output_path, sample_rate=16000):
         # Setting rate
         audio = audio.set_frame_rate(sample_rate).set_channels(1)
         audio.export(output_path, format="wav")
-    except Exception as e:
-        print(f"Error converting to WAV: {e}")
+    except CouldntDecodeError as e:
+        logging.error(f"Error converting to WAV: {e}")
 
 
 # Photo handler
@@ -83,26 +95,19 @@ async def handle_photo(event):
             with open(f'{folder_path}/photo_{len(files)}.jpg', 'wb') as photo:
                 photo.write(image_bytes)
                 await event.reply(f'The image containing faces and saved')
-        except Exception as e:
-            print(f"Error creating folder: {e}")
+        except OSError as e:
+            logging.error(f"Error creating folder: {e}")
     else:
         await event.reply(f'The image does not contain faces and will be skipped')
 
 
 async def has_faces(image_bytes):
-    ''' Function using 2 detection methods:
-    - dlib.get_frontal_face_detector()
-    - Haar's cascade
-    '''
     try:
         # Preparing image for detection
         image = cv2.imdecode(np.frombuffer(image_bytes, dtype=np.uint8), cv2.IMREAD_COLOR)
         gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
         # Trying to detect
-        faces = dlib_basic_detector(gray_image)
-        if not faces:
-            # Trying to use haar cascade
-            faces = haarcascade_detector(gray_image)
+        faces = face_detection(gray_image)
 
         if faces is not None and len(faces) > 0:
             if DEBUG_MODE:
@@ -110,15 +115,32 @@ async def has_faces(image_bytes):
             return True
 
     except Exception as e:
-        print("Exception raised:", e)
+        logging.error(f"Exception raised: {e}")
     return False
+
+
+def face_detection(gray_image: np.ndarray) -> Union[List[Tuple[int, int, int, int]], None]:
+    '''Function utilizing various face detection methods for scalability.
+
+    Args:
+        gray_image (np.ndarray): Grayscale image for face detection.
+
+    Returns:
+        Union[List[Tuple[int, int, int, int]], None]: A list of tuples representing
+        detected faces as (x, y, width, height) or None if no faces are found.
+    '''
+    faces = dlib_basic_detector(gray_image)
+    if not faces:
+        # Attempting to use Haar cascade
+        faces = haarcascade_detector(gray_image)
+    return faces
 
 
 def haarcascade_detector(gray_image):
     face_cascade = cv2.CascadeClassifier("Models/haarcascade_frontalface_default.xml")
     faces = face_cascade.detectMultiScale(gray_image, scaleFactor=1.04, minNeighbors=5)
     if faces is not None:
-        print(f"{len(faces)} faces found with Haar's cascade")
+        logging.info(f"{len(faces)} faces found with Haar's cascade")
     return faces
 
 
@@ -131,7 +153,7 @@ def dlib_basic_detector(gray_image):
     faces_coordinates = [(face.left(), face.top(), face.width(), face.height()) for face in faces]
 
     if faces_coordinates:
-        print(f"{len(faces_coordinates)} faces found with dlib_basic_detector")
+        logging.info(f"{len(faces_coordinates)} faces found with dlib_basic_detector")
 
     return faces_coordinates
 
@@ -148,7 +170,7 @@ def show_faces(image, gray_image, faces):
         predictor = dlib.shape_predictor('Models/shape_predictor_68_face_landmarks.dat')
         for x, y, w, h in faces:
             cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 3)
-            # creating the rectangle object from the outputs of haar cascade calssifier
+            # creating the rectangle object from the outputs of haar cascade classifier
             drect = dlib.rectangle(int(x), int(y), int(x + w), int(y + h))
             landmarks = predictor(gray_image, drect)
             points = shape_to_np(landmarks)
@@ -156,8 +178,8 @@ def show_faces(image, gray_image, faces):
                 x = i[0]
                 y = i[1]
                 cv2.circle(image, (x, y), 2, (0, 255, 0), -1)
-    plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
-    plt.show()
+        plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+        plt.show()
 
 
 if __name__ == '__main__':
